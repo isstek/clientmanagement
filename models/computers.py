c610649a24da1536c18bcdcfa8d1a52fa93544b3
Igal Milman <igalmilman@gmail.com>
2019-03-07 13:26:21 -0500

    fix(models): Increased Serial number size

--- models/computers.py
+++ models/computers.py
@@ -36,10 +36,10 @@ class Computer(networkequipment.NetworkEquipment):
     computername = models.CharField('Computer Name', max_length=30, null=False)
     operatingsystem = models.CharField(max_length=4, null=False, default='W10', blank=False, choices=OPERATING_SYSTEMS)
     manufacturer = models.CharField(max_length=1, null=False, default='D', blank=False, choices=MANUFACTURER)
-    model = models.CharField(max_length=35, null=True, default='', blank=True)
+    model = models.CharField(max_length=40, null=True, default='', blank=True)
     compmonth = models.IntegerField("Recieved Month (enter a number)", blank=True, null=True, default=None, validators=[MaxValueValidator(12), MinValueValidator(1)])
     compyear = models.IntegerField("Recieved Year (enter a number)", blank=True, null=True, default=None, validators=[MaxValueValidator(2030), MinValueValidator(2000)])
-    serialnumber = models.CharField(max_length=25, null=True, default='', blank=True)
+    serialnumber = models.CharField(max_length=40, null=True, default='', blank=True)
     user = models.ManyToManyField(person.Person, blank=True, related_name='computer')
     
     def __str__(self):
