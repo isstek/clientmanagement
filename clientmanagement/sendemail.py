7a3cd2120f07ef79e071132369d6b8f70436af40
Igal Milman <igalmilman@gmail.com>
2019-05-14 11:46:03 -0400

    fix(exceptions): Add exception handling in sending emails

--- clientmanagement/sendemail.py
+++ clientmanagement/sendemail.py
@@ -4,6 +4,7 @@ from django.utils.html import strip_tags
 from django.template import Context
 from django.contrib.auth.models import User
 
+logger = logging.getLogger(__name__)
 
 def sendemaileveryone(template, data, subject):
     if template[-3:] == 'htm':
@@ -33,67 +34,94 @@ def sendemailtouser(template, data, subject, user):
 def sendemaileveryonetxt(template, data, subject):
     plaintext = get_template(template)
     for user in User.objects.exclude(email__isnull=True).exclude(email__exact=''):
+        try:
+            data['user'] = user
+            text_content = plaintext.render(data)
+            msg = EmailMultiAlternatives(subject, text_content, '', [user.email])
+            msg.send()
+        except Exception as exc:
+            logger.error('Error in sendemaileveryonetxt.\nException: ', exc_info=True)
+
+def sendemailtoonetxt(template, data, subject, email, name):
+    try:
+        plaintext = get_template(template)
+        data['name'] = name
+        text_content = plaintext.render(data)
+        msg = EmailMultiAlternatives(subject, text_content, '', [email])
+        msg.send()
+    except Exception as exc:
+        logger.error('Error in sendemailtoonetxt.\nException: ', exc_info=True)
+
+def sendemailtosometxt(template, data, subject, email):
+    try:
+        plaintext = get_template(template)
+        text_content = plaintext.render(data)
+        msg = EmailMultiAlternatives(subject, text_content, '', email)
+        msg.send()
+    except Exception as exc:
+        logger.error('Error in sendemailtosometxt.\nException: ', exc_info=True)
+
+def sendemailtousertxt(template, data, subject, user):
+    try:
+        plaintext = get_template(template)
         data['user'] = user
         text_content = plaintext.render(data)
         msg = EmailMultiAlternatives(subject, text_content, '', [user.email])
         msg.send()
-
-def sendemailtoonetxt(template, data, subject, email, name):
-    plaintext = get_template(template)
-    data['name'] = name
-    text_content = plaintext.render(data)
-    msg = EmailMultiAlternatives(subject, text_content, '', [email])
-    msg.send()
-
-def sendemailtosometxt(template, data, subject, email):
-    plaintext = get_template(template)
-    text_content = plaintext.render(data)
-    msg = EmailMultiAlternatives(subject, text_content, '', email)
-    msg.send()
-
-def sendemailtousertxt(template, data, subject, user):
-    plaintext = get_template(template)
-    data['user'] = user
-    text_content = plaintext.render(data)
-    msg = EmailMultiAlternatives(subject, text_content, '', [user.email])
-    msg.send()
+    except Exception as exc:
+        logger.error('Error in sendemailtousertxt.\nException: ', exc_info=True)
 
 
 def sendemaileveryonehtml(template, data, subject):
     for user in User.objects.exclude(email__isnull=True).exclude(email__exact=''):
+        try:
+            data['user'] = user
+            html_content = render_to_string(template, data)
+            text_content = strip_tags(html_content)
+            msg = EmailMultiAlternatives(subject, text_content, '', [user.email])
+            msg.attach_alternative(html_content, "text/html")
+            msg.send()
+        except Exception as exc:
+            logger.error('Error in sendemaileveryonehtml.\nException: ', exc_info=True)
+
+def sendemailtoonehtml(template, data, subject, email, name):
+    try:
+        data['name'] = name
+        html_content = render_to_string(template, data)
+        text_content = strip_tags(html_content)
+        msg = EmailMultiAlternatives(subject, text_content, '', [email])
+        msg.attach_alternative(html_content, "text/html")
+        msg.send()
+    except Exception as exc:
+        logger.error('Error in sendemailtoonehtml.\nException: ', exc_info=True)
+
+def sendemailtosomehtml(template, data, subject, email):
+    try:
+        html_content = render_to_string(template, data)
+        text_content = strip_tags(html_content)
+        msg = EmailMultiAlternatives(subject, text_content, '', email)
+        msg.attach_alternative(html_content, "text/html")
+        msg.send()
+    except Exception as exc:
+        logger.error('Error in sendemailtosomehtml.\nException: ', exc_info=True)
+
+def sendemailtouserhtml(template, data, subject, user):
+    try:
         data['user'] = user
         html_content = render_to_string(template, data)
         text_content = strip_tags(html_content)
         msg = EmailMultiAlternatives(subject, text_content, '', [user.email])
         msg.attach_alternative(html_content, "text/html")
         msg.send()
-
-def sendemailtoonehtml(template, data, subject, email, name):
-    data['name'] = name
-    html_content = render_to_string(template, data)
-    text_content = strip_tags(html_content)
-    msg = EmailMultiAlternatives(subject, text_content, '', [email])
-    msg.attach_alternative(html_content, "text/html")
-    msg.send()
-
-def sendemailtosomehtml(template, data, subject, email):
-    html_content = render_to_string(template, data)
-    text_content = strip_tags(html_content)
-    msg = EmailMultiAlternatives(subject, text_content, '', email)
-    msg.attach_alternative(html_content, "text/html")
-    msg.send()
-
-def sendemailtouserhtml(template, data, subject, user):
-    data['user'] = user
-    html_content = render_to_string(template, data)
-    text_content = strip_tags(html_content)
-    msg = EmailMultiAlternatives(subject, text_content, '', [user.email])
-    msg.attach_alternative(html_content, "text/html")
-    msg.send()
+    except Exception as exc:
+        logger.error('Error in sendemailtouserhtml.\nException: ', exc_info=True)
 
 def sendemailhtml(template, data, subject, email):
-    html_content = render_to_string(template, data)
-    text_content = strip_tags(html_content)
-    msg = EmailMultiAlternatives(subject, text_content, '', [email])
-    msg.attach_alternative(html_content, "text/html")
-    msg.send()
+    try:
+        html_content = render_to_string(template, data)
+        text_content = strip_tags(html_content)
+        msg = EmailMultiAlternatives(subject, text_content, '', [email])
+        msg.attach_alternative(html_content, "text/html")
+        msg.send()
+    except Exception as exc:
+        logger.error('Error in sendemailhtml.\nException: ', exc_info=True)
